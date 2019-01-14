a79cec4f0c0c1bc202f75d7f289245278543c166
Ummarsyed <sj@tranquilitysoft.com>
2019-01-14 21:17:02 +0530

    monthly slots calender

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Image } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 const left_arrow = require('./src/icons/left_arrow.png');
 const kabab_memu = require('./src/icons/kabab_memu.png');
 export default class App extends React.Component {
   render() {
     return (
-      <View >
+      <View style={styles.container}>
         <StatusBar
           barStyle="light-content"
           backgroundColor="#388e3c"
         />
 
-        <View style={styles.container}>
+        <View>
           <View style={styles.topNav} >
             <View style={[styles.colStyle, styles.dailySelect]} >
               <Text style={styles.textColor}>Daily</Text>
@@ -32,15 +33,52 @@ export default class App extends React.Component {
               <View style={styles.circleAllAvailable}><Text style={styles.whiteText14}>center</Text></View>
             </View>
           </View>
+          <View style={{ marginHorizontal: 16, marginVertical: 16, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+            <View style={styles.avilableSmall}></View>
+            <Text style={{ marginHorizontal: 8, }}>Available</Text>
+            <View style={styles.notavilableSmall}></View>
+            <Text>Not Available</Text>
+
+          </View>
+
+          <View style={{ marginHorizontal: 16, }}>
+            <Text style={styles.blachBoldText16}>Select Dates</Text>
+
+            <Calendar
+              // Date marking style [simple/period/multi-dot/single]. Default = 'simple'
+              markingType={'custom'}
+              markedDates={{
+                '2018-03-28': {
+                  customStyles: {
+                    container: {
+                      backgroundColor: 'green',
+                    },
+                    text: {
+                      color: 'black',
+                      fontWeight: 'bold'
+                    },
+                  },
+                },
+                '2018-03-29': {
+                  customStyles: {
+                    container: {
+                      backgroundColor: 'white',
+                      elevation: 2
+                    },
+                    text: {
+                      color: 'blue',
+                    },
+                  }
+                }
+              }}
+            />
+          </View>
+          <View style={{bottom:0,borderColor:'green',borderWidth:2,position:''}}>
+            <Text>Bottom</Text>
+          </View>
         </View>
 
-        <View style={{ marginHorizontal: 16, marginVertical: 16, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
-          <View style={styles.avilableSmall}></View>
-          <Text style={{marginHorizontal: 8,}}>Available</Text>
-          <View style={styles.notavilableSmall}></View>
-          <Text>Not Available</Text>
 
-        </View>
 
       </View >
 
@@ -53,18 +91,19 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FFFFFF',
     fontFamily: 'Roboto',
-    justifyContent: 'center',
-    alignItems: 'center',
+    minHeight: '100%',
   },
   topNav: {
+    backgroundColor: '#FFFFFF',
     flexDirection: 'row',
     height: 56,
     marginHorizontal: 16,
     marginVertical: 16,
   },
   colStyle: {
-    width: '35%',
+    width: '33%',
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
